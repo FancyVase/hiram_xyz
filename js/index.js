364d0118ffcf5d615dc09293c0645b8d5dca7a15
Hiram Moncivais <mm.hiram@gmail.com>
2016-02-23 02:12:09 -0500

    Add unique images

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
 	var offset = $('#header').height();
-	console.log(offset);
 
 	$('.arrow').show();
 
 	$(window).scroll(function() {
-		console.log($(window).scrollTop())
 		if ($(window).scrollTop() > 80) {
 			$('.arrow').hide();
 		} else {

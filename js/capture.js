76ffec0e73715653613b212df62d67fc42b1b021
Hiram Moncivais <mm.hiram@gmail.com>
2016-03-01 02:37:20 -0500

    Add navbar to Capture and Design pages

--- js/capture.js
+++ js/capture.js
@@ -8,3 +8,13 @@ var $grid = $('.grid').masonry({
 $grid.imagesLoaded().progress( function() {
 	$grid.masonry();
 });  
+
+$(document).ready(function() {
+	$(window).scroll(function() {
+		if ($(window).scrollTop() < $('.grid-item').offset().top) {
+	    	$('.nav').removeClass('nav-down').addClass('nav-up');
+	    } else {
+	    	$('.nav').removeClass('nav-up').addClass('nav-down');
+	    }
+	});
+});

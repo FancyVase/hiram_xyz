45f5cef5dfa8cc033a327ee85bf900d63b9e7944
Hiram Moncivais <mm.hiram@gmail.com>
2016-03-05 15:05:53 -0500

    Add navbar to Design and Capture

--- js/capture.js
+++ js/capture.js
@@ -9,12 +9,12 @@ $grid.imagesLoaded().progress( function() {
 	$grid.masonry();
 });  
 
-$(document).ready(function() {
-	$(window).scroll(function() {
-		if ($(window).scrollTop() < $('.grid-item').offset().top) {
-	    	$('.nav').removeClass('nav-down').addClass('nav-up');
-	    } else {
-	    	$('.nav').removeClass('nav-up').addClass('nav-down');
-	    }
-	});
-});
+// $(document).ready(function() {
+// 	$(window).scroll(function() {
+// 		if ($(window).scrollTop() < $('.grid-item').offset().top) {
+// 	    	$('.nav').removeClass('nav-down').addClass('nav-up');
+// 	    } else {
+// 	    	$('.nav').removeClass('nav-up').addClass('nav-down');
+// 	    }
+// 	});
+// });
